a7b473cb1048539ff15bbbe64eae1c22a1abd182
Mike Douglas <1015725+mike-douglas@users.noreply.github.com>
2022-04-30 09:18:55 -0400

    Adding --background to push

--- push.sh
+++ push.sh
@@ -3,7 +3,8 @@
 PIXLET=/bin/pixlet
 
 ${PIXLET} render ${1} && \
-  ${PIXLET} push --api-token ${PIXLET_API_TOKEN} \
+  ${PIXLET} push --background \
+                 --api-token ${PIXLET_API_TOKEN} \
                  --installation-id ${PIXLET_INSTALLATION_ID} \
                  ${PIXLET_DEVICE_ID} \
                  `dirname ${1}`/`basename ${1} .star`.webp
