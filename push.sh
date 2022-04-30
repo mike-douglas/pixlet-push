5e96fc75606df0fa0efaad492274ceff94526afc
Mike Douglas <1015725+mike-douglas@users.noreply.github.com>
2022-04-30 08:31:59 -0400

    Fixing push script

--- push.sh
+++ push.sh
@@ -6,4 +6,4 @@ ${PIXLET} render ${1} && \
   ${PIXLET} push --api-token ${PIXLET_API_TOKEN} \
                  --installation-id ${PIXLET_INSTALLATION_ID} \
                  ${PIXLET_DEVICE_ID} \
-                 `basename ${1} .star`.webp
+                 `dirname ${1}`/`basename ${1} .star`.webp
